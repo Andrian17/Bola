88b876e3afea9d900d03781125b9121b7b465355
Andrian17 <andriancimen123@gmail.com>
2021-10-15 17:51:13 +0800

    perbaikan

--- js/script2.js
+++ js/script2.js
@@ -1,117 +1,64 @@
-
 function ambilData() {
-    $.getJSON('https://www.scorebat.com/video-api/v3/', function(data){
+  $.getJSON("https://www.scorebat.com/video-api/v3/", function (data) {
     let info = data.response;
     //console.log(data.response.title);
     $.each(info, function (i, data) {
-        
-        //console.log(data.videos);
-        //console.log( "ddffdf: " +data.videos.length)
-        if (i<=20) {
-            
-            $('#adr1').append(`<div class="card mb-3 adr1">
-            <span class"justify-content-center m-3"> No. `+ (parseInt(i)+1) +`</span>
-            <span class="d-flex justify-content-center mt-3">` + data.title + `</span>
-            <span class="d-flex justify-content-center">` + data.competition + `</span>
-            <span class="d-flex justify-content-center">` + data.date + `</span>
-            <div id="forVideo" class="mb-3 mt-3 w-50 p-3 m-auto border ">` + data.videos[0].embed + `</div>
-            </div>`);
-
-        }
+      if (i <= 20) {
+        $("#adr1").append(
+          `<div class="card mb-3">
+          <h5 class="mt-3">` +
+            data.title +
+            `</h5>
+            <div id="forVideo" class="mb-3 mt-3">` +
+            data.videos[0].embed +
+            `</div>
+            <h5 class="">` +
+            data.competition +
+            `</h5>
+            <h5 class="">` +
+            data.date +
+            `</h5>
+            </div>`
+        );
+      }
     });
-
-
-});
+  });
 }
 
-    let adr = document.getElementById('adr1');
-    adr.addEventListener('mouseover', function(e) {
-        e.target.classList.add('border-primary');
+let adr = document.getElementById("adr1");
+adr.addEventListener("mouseover", function (e) {
+  e.target.classList.add("border-primary");
+});
 
-    });
-
-    adr.addEventListener('mouseout', function(e){
-        e.target.classList.remove('border-primary');
-    })
-
-
+adr.addEventListener("mouseout", function (e) {
+  e.target.classList.remove("border-primary");
+});
 
 ambilData();
 
-
-
-$('.list-video').on('click', function(e){
-    let content;
-    $.getJSON('https://www.scorebat.com/video-api/v3/', function(data){
+$(".list-video").on("click", function (e) {
+  let content;
+  $.getJSON("https://www.scorebat.com/video-api/v3/", function (data) {
     let info = data.response;
     console.log(info[1].videos[0].title);
-    $.each(info, function(j, data){
-        content += `<div class="">` + data.videos[j].title + `</div> <div class="">` + data.videos[j].embed + `</div>` 
+    $.each(info, function (j, data) {
+      content +=
+        `<div class="">` +
+        data.videos[j].title +
+        `</div> <div class="">` +
+        data.videos[j].embed +
+        `</div>`;
     });
 
-    $('#coba').html(content);
-
-
+    $("#coba").html(content);
+  });
 });
-
-    
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // function tampikanSemuaMenu() {
 //     $.getJSON('data/pizza.json', function (data) {
 //         let menu = data.menu;
 //        // console.log(menu);
-//        $.each(menu, function (i, data) {    
+//        $.each(menu, function (i, data) {
 //           $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"><img src="img/pizza/' + data.gambar + '" class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
 //        });
 
@@ -123,12 +70,12 @@ $('.list-video').on('click', function(e){
 // $('.nav-link').on('click', function() {
 //     //semual class dihilangkan
 //      $('.nav-link').removeClass('active');
-//      //class yang di klik 
+//      //class yang di klik
 //      $(this).addClass('active');
 
 //      //DOM
 //      let kategori = $(this).html();
-     
+
 //      $('h1').html(kategori);
 
 //      //tampikanSemuaMenu
